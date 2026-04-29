69bd7f04d3eca0765fb7435adbe6f0342fa14164
João Pedro Rocha <jpfr2004@gmail.com>
2026-04-29 17:53:47 -0300

    Finalizando a geracao de Jsons

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
 function carregarTarefas() {
     const tarefa = document.getElementById("task-input");
 
-    if(tarefa.value.trim() === "") {
+    if (tarefa.value.trim() === "") {
         alert("Por favor, insira uma tarefa.");
         return;
     }
-    else{
 
-        const novaTarefa = {
-            descricao: tarefa.value,
-            concluido: false,
-            prioridade: "Alta"
-        };
+    const novaTarefa = {
+        descricao: tarefa.value,
+        concluido: false,
+        prioridade: "Alta"
+    };
 
-        const tarefaJSON = JSON.stringify(novaTarefa); //Transformando o objeto em JSON
+    fetch('http://127.0.0.1:5000/tarefas', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novaTarefa)
+    })
+    .then(response => response.json())
+    .then(dadosretornados => { // TUDO QUE DEPENDE DO BANCO FICA AQUI DENTRO!
+        console.log("Id vindo do banco: " + dadosretornados.id);
 
         const lista = document.getElementById("task-list");
         const item = document.createElement("li");
+        
+        item.setAttribute("data-id", dadosretornados.id);  //Guardando o id do banco como atributo do item para futuras operações (ex: delete)
+
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
 
         const textoTarefa = document.createElement("span");
-        
+        textoTarefa.textContent = novaTarefa.descricao; // Usando a descrição do objeto
+
         const botaoDeletar = document.createElement("button");
         botaoDeletar.textContent = "🗑️";
         botaoDeletar.className = "botao-deletar";
 
         botaoDeletar.onclick = function() {
+            // No futuro, aqui chamaremos o fetch de DELETE usando o data-id
             lista.removeChild(item); 
         };
-
-        textoTarefa.textContent = tarefa.value;
 
         item.appendChild(checkbox);
         item.appendChild(textoTarefa);
         item.appendChild(botaoDeletar);
         lista.appendChild(item);
+
         tarefa.value = "";
-    }
+    })
+    .catch(erro => {
+        console.error("Erro na comunicação com o servidor:", erro);
+        alert("Erro ao salvar no banco de dados.");
+    });
 }
